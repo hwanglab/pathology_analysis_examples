14e010c61923419f8306c3053b12a7acd66ac7f2
xhm1014 <mxu@ualberta.ca>
2020-07-30 09:36:12 -0400

    commit

---
+++ Example_03.py
@@ -0,0 +1,9 @@
+'''
+Exercise:
+let us use the example slide: TCGA-BL-A3JM-01Z-00-DX1.33E53972-CEA4-4D84-A5D2-7DAD7B0C27F8.svs'
+
+Tasks:
+Given the example slide,
+(1) divide the WSI into a set of non-overlapping blocks (or tiles), where each block should have the size of 256um by 256um
+(2) save the divided blocks into your local disk
+'''
